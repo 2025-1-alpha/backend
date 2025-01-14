701a2734d74f2a73fdf3416b3a765dcadb2b46c2
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-01-14 11:08:23 +0900

    init: 초기 세팅

---
+++ src/main/java/com/geulowup/backend/GeulowupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.geulowup.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeulowupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeulowupBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geulowup/backend/GeulowupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geulowup.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeulowupBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
