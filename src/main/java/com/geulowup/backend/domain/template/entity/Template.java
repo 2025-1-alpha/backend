abaaf41ab79dcad00f6b5f7d2281cb9aa6c29d31
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-03-26 16:22:17 +0900

    feat: 각 DB 테이블 별 JPA Entity 생성 - #6

---
+++ src/main/java/com/geulowup/backend/domain/template/entity/Template.java
@@ -0,0 +1,58 @@
+package com.geulowup.backend.domain.template.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import com.geulowup.backend.domain.user.entity.User;
+
+@Entity
+@Table(name = "templates")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Template {
+    @Id
+    @Column(name = "template_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User author;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String content;
+
+    @Column(name = "like_count", nullable = false)
+    private Integer likeCount;
+
+    private String keywords;
+
+    @Column(name = "is_private", nullable = false, columnDefinition = "TINYINT(1)")
+    private Boolean isPrivate = false;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
